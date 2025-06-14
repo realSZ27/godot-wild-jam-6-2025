dc6e6e4ba5cf3dbcdec5a5d07a559c90c04a6184
defeatedPond <raidyn.bilotta@gmail.com>
2025-06-14 17:32:48 -0500

    added a togglable hotbar. not function yet

--- Scripts/hotbar.gd
+++ Scripts/hotbar.gd
@@ -1,1 +1,17 @@
 extends Control
+
+@onready var buttons := $PanelContainer/HBoxContainer.get_children()
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed and !event.is_echo():
+		var index = event.keycode - KEY_1 
+		if index >= 0 and index < buttons.size():
+			toggle_slot(index)
+
+func toggle_slot(i: int) -> void:
+	buttons[i].button_pressed = !buttons[i].button_pressed
+	for index in range(buttons.size()):
+		if index != i:
+			buttons[index].button_pressed = false 
+	emit_signal("active_ability", i)
