fa032f8a39409bb989715f2fdb6e87081a810169
realSZ27 <karstenandmicaiah@gmail.com>
2025-06-14 10:14:31 -0500

    fix light check order

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -5,12 +5,19 @@ extends CharacterBody2D
 @onready var ray := $RayCast2D
 var eye_sight_distance := 400.0
 var fov = PI / 2
-var speed := 200.0
+var speed := 100.0
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
 
 func _physics_process(delta: float) -> void:
+	ray.target_position = ray.to_local(player.global_position)
+	
+	# turns off lights so you cant see them through walls
+	if ray.get_collider() == player:
+		$Lights.visible = true
+	else:
+		$Lights.visible = false
 	
 	var vector_distance = player.global_position - global_position
 	if  vector_distance.length() > 400:
@@ -19,22 +26,13 @@ func _physics_process(delta: float) -> void:
 	if vector_distance.length() < 150:
 		chase(vector_distance, delta)
 	
-	
 	# this is bullshit and made up i spent like 1000 hours trying to figure this out 
 	# everything else was easy its just this i hate angles so much 
 	# trigonometry can go fuck itself
 	#if (Vector2.RIGHT.rotated(rotation)).angle_to(vector_distance) > (fov / 2):
 	#	return
 	
-	
 	# makes it so they cant see you through walls n shit
-	ray.target_position = ray.to_local(player.global_position)
-	
-	if ray.get_collider() == player:
-		$Lights.visible = true
-	else:
-		$Lights.visible = false
-	
 	if !ray.is_colliding() or ray.get_collider() != player:
 		return
 	
