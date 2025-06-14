dfe8236666b348eb40f684c7c18b0294bca671e7
StinkyFettish <alexjaejoon@gmail.com>
2025-06-14 18:31:01 -0500

    Main menu with a blank options page

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Wall.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	get_tree().change_scene_to_file("res://Scenes/options.tscn")
+	pass # Replace with function body.
