2f16f40c1c4bd46e86ec201edb83a97554084588
realSZ27 <karstenandmicaiah@gmail.com>
2025-06-15 00:02:51 -0500

    added detection meter

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -6,12 +6,14 @@ extends CharacterBody2D
 var eye_sight_distance := 400.0
 var fov = PI / 2
 var speed := 100.0
+var is_cooldown := false
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("Player")
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta) -> void:
 	ray.target_position = ray.to_local(player.global_position)
+	var current_frame = $AnimatedSprite2D.frame;
 	
 	#turns off lights so you cant see them through walls
 	if ray.get_collider() == player:
@@ -21,6 +23,10 @@ func _physics_process(delta: float) -> void:
 	
 	var vector_distance = player.global_position - global_position
 	if  vector_distance.length() > 400:
+		if !is_cooldown and current_frame > 0:
+			$AnimatedSprite2D.frame = current_frame - 1
+			print("changed frame down")
+			start_cooldown()
 		return
 	elif vector_distance.length() < 55:
 		get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
@@ -34,8 +40,18 @@ func _physics_process(delta: float) -> void:
 	if !ray.is_colliding() or ray.get_collider() != player:
 		return
 	
-	chase(vector_distance)
+	if !is_cooldown and current_frame < 21:
+		$AnimatedSprite2D.frame = current_frame + 1
+		print("changed frame up")
+		start_cooldown()
 	
+	if current_frame == 21:
+		chase(vector_distance)
+		
+func start_cooldown():
+	is_cooldown = true
+	$Timer.start()
+
 func chase(vector_distance) -> void:
 	# this works fine it just looks a bit jittery when youre super upclose
 	# obv that wouldnt happen during the game but just wanted to mention
@@ -44,8 +60,12 @@ func chase(vector_distance) -> void:
 		velocity = Vector2.ZERO
 		return
 	
-	velocity = vector_distance / vector_distance.length() * speed  
+	velocity = vector_distance / vector_distance.length() * speed
 	move_and_slide()
 	# should change how they look at you in the future since this makes
 	# it look kinda janky, but it works for now
-	look_at(player.global_position)
+	#look_at(player.global_position)
+
+func _on_timer_timeout():
+	is_cooldown = false
+	print("timer done")
--- Scripts/movement.gd
+++ Scripts/movement.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var speed = 400
 @onready var camera = $Camera2D
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta) -> void:
 	#look_at(get_global_mouse_position())
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
